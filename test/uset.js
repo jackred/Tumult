4fdc14569f8a37e410b76fd48fde104824b7b721
Jackred <jackred@tuta.io>
2020-11-13 08:24:12 +0000

    UPDATE License mention in code: was wrong, default comment to MIT

--- test/uset.js
+++ test/uset.js
@@ -1,8 +1,8 @@
 //-*- Mode: js2; js2-basic-offset: 2;  tab-width: 2; -*-
-//vim:set et sts=4 ts=4 tw=80:
-//This Source Code Form is subject to the terms of the MIT License.
-//If a copy of the ML was not distributed with this
-//file, You can obtain one at https://opensource.org/licenses/MIT
+//This Source Code Form is subject to the terms of the Apache 2.0 License.
+//If a copy of the A2 was not distributed with this
+//file, You can obtain one at https://opensource.org/licenses/Apache-2.0
+
 //author: JackRed <jackred@tuta.io>
 'use strict';
 
